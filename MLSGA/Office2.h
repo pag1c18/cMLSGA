16840e7f87befbb4c880eef6a208c2caab3640bc
Przemyslaw Grudniewski <pag1c18@soton.ac.uk>
2019-04-15 15:59:25 +0100

    *15/04/2019* - MLSGA-framework PUBLIC

--- MLSGA/Office2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//#import "C:\Program Files (x86)\Common Files\Microsoft Shared\OFFICE15\MSO.DLL" \
-rename("DocumentProperties", "DocumentPropertiesXL") \
-rename("RGB", "RBGXL")
-
-//#import "C:\Program Files (x86)\Common Files\Microsoft Shared\VBA\VBA6\VBE6EXT.OLB"
-
-
